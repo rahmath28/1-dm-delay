f3cf06bccd68a7a9e0a097c6f64f9a25180c0e90
rahmath28 <rahmath8525@gmail.com>
2025-08-13 09:17:49 +0530

    displayed both message send and delete , removed both msgs in 5 secs by settimeout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,32 +10,63 @@ const Form = () => {
     // typescript use panna , states ku type enna nu mention pannanum : useState<string>("")
 
     const [text, setText] = useState<string>(""); // this state is for : to get message.
-    const [delay,setDelay] = useState<number>(0); // this state is for : input delay seconds.
+    const [delay,setDelay] = useState<number>(0); // this state is for : input delay ku na get pandra  numbers store panna.
     const [isSending, setIsSending] = useState<boolean>(false) // this state is for : message sent or not, initially not : reason nothing send.
-    const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null); // this state is for : input delay seconds.
-    const [sendMessage, SetSendMessage]= useState<string>("")
+    const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null); // this state is for :  settimeout function works or not , 
+    // created a settimeout function in  id variable, 
+    // settimeout function nadantha timerId state la  update pandren.. 
+    const [sendMessage, SetSendMessage]= useState<string>("") // finally send agura message..
+    const [deletedmessage , setDeletedMessage] = useState<string>("")
 
-    // functions
-   const handleSubmit = () =>{
-    //sending message , so true the state 
-    setIsSending(true);
+  // handle submit
+   const handleSubmit = () => {
+     //sending message , so true the state 
+  setIsSending(true);
 
-    // next delay set pannanum
-    const id = setTimeout(() =>{
-        SetSendMessage(text);
-        setText("");
-        setDelay(0);
-        setIsSending(false);
+     // next delay set pannanum
+  const id = setTimeout(() => {
+    SetSendMessage(text);
+    setText("");
+    setDelay(0);
+    
+    // SetSendMessage use panni message send panniyachuna submit pannumbothu , setIsSending false pandrom.
+    setIsSending(false);
 
-    }, delay * 1000) 
+    // hide the sent message after 5 seconds
+    setTimeout(() => {
+      SetSendMessage("");
+    }, 5000);
+  }, delay * 1000);
     //  delay number ah get panni , 1000 milliseconds oda multiply panni,
     //  message send panndrom , send panna aprm mesage state ah empty,
     // kuruppita time rannge nale atha settimeout function kulla , logic build pandrom ,
     // antha settimeout function ah id dra variable la create panni timerIs state la pass pandrom , 
     // antha id variable than message send pannuthu , after a delay
-    
-    setTimerId(id)
-   }
+
+  setTimerId(id);
+};
+
+
+
+//    cancel message
+// cancel message
+const handleCancel = () => {
+  if (timerId) clearTimeout(timerId);
+  setIsSending(false);
+
+  // set deleted message
+  setDeletedMessage(text);
+
+  // clear input and delay
+  setText("");
+  setDelay(0);
+
+  // hide the deleted message after 2 seconds
+  setTimeout(() => {
+    setDeletedMessage("");
+  }, 5000);
+};
+
 
   return (
     <div className="bg-white-50 rounded p-4 border-2 space-y-4">
@@ -43,26 +74,38 @@ const Form = () => {
 
         <Textarea placeholder="Write your message..." 
         onChange={(e) => setText(e.target.value)} value={text}/>
+        {/* submit pannumpothu value ah get pannanum so , value la text state kudathachu.. */}
 
          {/* {text} : updated my message in text state by onchange event*/} 
 
         <Input type="number" placeholder="Delay in seconds" value={delay} 
-        onChange={(e) => setDelay(Number(e.target.value))}/>
+        onChange={(e) => setDelay(Number(e.target.value))} disabled ={isSending}/>
+        {/* form la irunthu vara data string ah irukkum atha number ah get pannanum.. :  setDelay(Number(e.target.value)) */}
+        {/*submit pannumpothu delay number value ah get pannanum so , value la delay state kudathachu..  */}
 
         {/* {delay} : got delay */}
        
-        <Button className="w-full" onClick={handleSubmit}>
-            Send a message
-        </Button>
+       {/* isSending :  na initially false , ( !isSending ) >> not isSending  na true  */}
+       {/* !isSending : means true , apo submit pannanum , so handle click button..*/}
+       {/* isSending : which means false , na habdle cancel */}
+        {!isSending ?  <Button className="w-full" onClick={handleSubmit}>  Send with delay </Button>
+        :  <Button className="w-full" variant="destructive" onClick={handleCancel}> Cancel message</Button> }
 
         {/* {sendMessage} : got message  */}
 
+        {/* isSending : tue na enakku sendMessage state la message irukku , atha base panni logic */}
         {/* : this logic will work if only the message is sent.. */}   
         {sendMessage &&
         <div className="bg-green-200 p-2 rounded ">
-           <p> Message sent : {sendMessage}</p>
+           <p className="text-green-800"> Message sent : {sendMessage}</p>
         </div>
         } 
+
+        {deletedmessage  &&
+          <div className="bg-red-200 p-2 rounded">
+            <p className="text-red-800">Deleted Mesage : {deletedmessage}</p>
+          </div>
+        }
         
         
     </div>
